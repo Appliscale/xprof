7fb1da1f16a93267cc1660c87be79c6c11f12e80
Péter Gömöri <gomoripeti@gmail.com>
2017-01-20 12:05:16 +0100

    Properly handle quoted atoms in get_available_funs
    Don’t use list_to_atom/atom_to_list as they don’t handle atoms with
    special characters or quotes properly. Also avoid creating new atoms.
    
    Still atoms that don't need to be quoted are only recognised unquoted.

--- test/xprof_vm_info_tests.erl
+++ test/xprof_vm_info_tests.erl
@@ -12,7 +12,7 @@ get_available_funs_test_() ->
              ?assertEqual([], L2 -- L1),
              ?assertNotEqual([], L1 -- L2)
      end,
-     {"Only global function are listed",
+     {"Only global functions are listed",
       fun() ->
               L1 = ?M:get_available_funs(<<"xprof_vm_info">>),
               ?assert(lists:member([?M, get_available_funs, 1], L1)),
@@ -52,4 +52,67 @@ get_available_funs_test_() ->
               ?assertEqual([[?M, get_available_funs, 1]], L2),
               L3 = ?M:get_available_funs(<<"xprof_vm_info:get_available_funs/99">>),
               ?assertEqual([], L3)
-      end}].
+      end},
+     {"Match-spec fun matching",
+      fun() ->
+              L1 = ?M:get_available_funs(<<"xprof_vm_info:get_available_funs(_) ->">>),
+              ?assertEqual([[?M, get_available_funs, 1]], L1),
+              L2 = ?M:get_available_funs(<<"xprof_vm_info:get_available_funs message(get_tcw())">>),
+              ?assertEqual([[?M, get_available_funs, 1]], L2)
+      end}
+    ].
+
+weird_atoms_test_() ->
+    {setup,
+     fun load_weird_mod/0,
+     fun code:purge/1,
+     [
+      {"Module name with special character",
+       fun() ->
+               L1 = ?M:get_available_funs(<<"'A.B">>),
+               ?assertEqual([['A.B.C', h, 10]], L1),
+               L2 = ?M:get_available_funs(<<"'A.B.C':">>),
+               ?assertEqual([['A.B.C', 'f?', 0],
+                             ['A.B.C', 'g\'g', 0],
+                             ['A.B.C', h, 1],
+                             ['A.B.C', h, 10],
+                             ['A.B.C', module_info, 0],
+                             ['A.B.C', module_info, 1]
+                            ], L2)
+       end},
+      {"Function name with special character",
+       fun() ->
+               L1 = ?M:get_available_funs(<<"'A.B.C':'f">>),
+               ?assertEqual([['A.B.C', 'f?', 0]], L1),
+               L2 = ?M:get_available_funs(<<"'A.B.C':'g">>),
+               ?assertEqual([['A.B.C', 'g\'g', 0]], L2)
+       end},
+      {"Multiple arity matches",
+       fun() ->
+               L1 = ?M:get_available_funs(<<"'A.B.C':h/1">>),
+               ?assertEqual([['A.B.C', h, 1], ['A.B.C', h, 10]], L1)
+       end}
+     ]}.
+
+load_weird_mod() ->
+    %% h/10 calls the other local functions
+    %% that would be eliminated by the compiler otherwise
+    Str =
+        "-module('A.B.C').\n"
+        "-export([h/10]).\n"
+        "'f?'() -> ok.\n"
+        "'g\\'g'() -> ok.\n"
+        "h(_1) -> ok.\n"
+        "h(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10) ->"
+        " {'f?'(), 'g\\'g'(), h(1)}.\n",
+    Forms = scan_and_parse(Str, {1,1}),
+    {ok, Mod, Bin} = compile:forms(Forms, []),
+    {module, Mod} = code:load_binary(Mod, [], Bin),
+    Mod.
+
+scan_and_parse("", _) -> [];
+scan_and_parse(Str, StartLoc) ->
+    {done, {ok, Tokens, EndLoc}, Rest} =
+        erl_scan:tokens([], Str, StartLoc),
+    {ok, Form} = erl_parse:parse_form(Tokens),
+    [Form|scan_and_parse(Rest, EndLoc)].
