2ca58f6c7071eede12a725f30fc70e457bd564e2
Pawel Pikula <ppikula@gmail.com>
2017-02-19 20:43:34 +0100

    Externalize strings and reafctor call_tracer_tests a bit

--- priv/test/call_tracer.test.jsx
+++ priv/test/call_tracer.test.jsx
@@ -4,62 +4,65 @@ import React from "react";
 import ReactTestUtils from "react-addons-test-utils";
 import { shallow } from "enzyme";
 import { expect } from "chai";
-
 import { CallsTracer, CallsTableRow } from "../app/call_tracer.jsx";
-
 
 function mockItem() {
   return { call_time: 10, pid: "<0.1.0>", args: "[]", res: "result" };
 }
 
-// TODO before each
 describe("<CallsTableRow />", () => {
-  let el = null;
+  let row = null;
+  const EXPAND_COLLAPSE_TOGGLE = ".btn";
+  const EXPANDED_ROW = "row-expanded";
+  const COLLAPSED_ROW = "row-normal";
+  const EXPAND_BTN_CLASS = ".glyphicon-chevron-right";
+  const COLLAPSE_BTN_CLASS = ".glyphicon-chevron-down";
+
   beforeEach(() => {
-    el = shallow(<CallsTableRow key="1" item={mockItem()}/>);
+    row = shallow(<CallsTableRow key="1" item={mockItem()}/>);
   });
 
   it("is a table row", function() {
-    expect(el.name()).to.equal("tr");
+    expect(row.name()).to.equal("tr");
   });
 
   it("is not exapanded and has expand button in the collapsed mode", () => {
-    expect(el.prop("className")).to.equal("row-normal");
-    expect(el.find(".glyphicon-chevron-right").exists()).to.equal(true);
+    expect(row.prop("className")).to.equal(COLLAPSED_ROW);
+    expect(row.find(EXPAND_BTN_CLASS).exists()).to.equal(true);
   });
 
   it("has call time column", () => {
-    expect(el.contains(<td>{10} &micro;s</td>)).to.equal(true);
+    expect(row.contains(<td>{10} &micro;s</td>)).to.equal(true);
   });
 
   it("has pid column", () => {
-    expect(el.contains(<td>{"<0.1.0>"}</td>)).to.equal(true);
+    expect(row.contains(<td>{"<0.1.0>"}</td>)).to.equal(true);
   });
 
   it("has args column with codebox", () => {
-    expect(el.contains("[]")).to.equal(true);
+    expect(row.contains("[]")).to.equal(true);
   });
 
   it("has result column with codebox", () => {
-    expect(el.contains("result")).to.equal(true);
+    expect(row.contains("result")).to.equal(true);
   });
 
   it("expands after clicking expand button", () => {
-    let el2 = shallow(<CallsTableRow key="1" item={mockItem()}/>);
-    el2.find(".btn").simulate("click", { preventDefault() {} });
+    let row2 = shallow(<CallsTableRow key="1" item={mockItem()}/>);
+    row2.find(EXPAND_COLLAPSE_TOGGLE).simulate("click", { preventDefault() {} });
 
-    expect(el2.find(".glyphicon-chevron-down").exists()).to.equal(true);
-    expect(el2.prop("className")).to.equal("row-expanded");
+    expect(row2.find(COLLAPSE_BTN_CLASS).exists()).to.equal(true);
+    expect(row2.prop("className")).to.equal(EXPANDED_ROW);
   });
 
   it("collapses after clicking button twice", () => {
-    let el2 = shallow(<CallsTableRow key="1" item={mockItem()}/>);
+    let row2 = shallow(<CallsTableRow key="1" item={mockItem()}/>);
 
-    el2.find(".btn").simulate("click", { preventDefault() {} });
-    el2.find(".btn").simulate("click", { preventDefault() {} });
+    row2.find(EXPAND_COLLAPSE_TOGGLE).simulate("click", { preventDefault() {} });
+    row2.find(EXPAND_COLLAPSE_TOGGLE).simulate("click", { preventDefault() {} });
 
-    expect(el2.find(".glyphicon-chevron-right").exists()).to.equal(true);
-    expect(el2.prop("className")).to.equal("row-normal");
+    expect(row2.prop("className")).to.equal(COLLAPSED_ROW);
+    expect(row2.find(EXPAND_BTN_CLASS).exists()).to.equal(true);
   });
 
 });
