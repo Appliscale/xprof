74d76cf328e75c48ab9c69cc80bf9fde85403c66
Marcin Kasprowicz <kasprowicz23@gmail.com>
2017-11-23 11:40:27 +0100

    Remove new-priv/ from root

--- new-priv/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export * from './UIConstants';
-export * from './TracingConstants';
-export * from './ApiConstants';
-export * from './PoolingConstants';
-export * from './GraphConstants';
