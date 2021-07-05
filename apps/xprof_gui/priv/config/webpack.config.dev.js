01de18cc0b1b5f21dc82baeaa98ab76562fdcdfa
Péter Gömöri <gomoripeti@gmail.com>
2021-07-05 02:17:44 +0200

    Update configs to webpack 5
    - https://webpack.js.org/migrate/5/#update-outdated-options
    - https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5/64802753#64802753
      https://webpack.js.org/migrate/5/#clean-up-configuration

--- apps/xprof_gui/priv/config/webpack.config.dev.js
+++ apps/xprof_gui/priv/config/webpack.config.dev.js
@@ -227,8 +227,6 @@ module.exports = {
       inject: true,
       template: paths.appHtml,
     }),
-    // Add module names to factory functions so they appear in browser profiler.
-    new webpack.NamedModulesPlugin(),
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
     new webpack.DefinePlugin(env.stringified),
@@ -250,14 +248,20 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
+  optimization: {
+    // Add module names to factory functions so they appear in browser profiler.
+    moduleIds: 'named',
+  },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+    },
   },
   // Turn off performance hints during development because we don't do any
   // splitting or minification in interest of speed. These warnings become
